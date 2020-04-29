7ab714c789172232cd609e2c0d7a8ee87d35e5a6
Kaveh <afroukhtehk@gmail.com>
2020-04-28 21:43:32 -0400

    deploybuild

--- models/todo.model.js
+++ models/todo.model.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.set('useFindAndModify', false)
-const url = 'mongodb+srv://root:b3nq258@cluster1-rbdyf.mongodb.net/test?retryWrites=true&w=majority'
+const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
